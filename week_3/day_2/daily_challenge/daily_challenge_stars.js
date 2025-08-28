93738acaeba582a5e0ca321489cf0b5fd1f817c0
Aznag00 <125455539+Aznag00@users.noreply.github.com>
2025-08-28 15:49:58 +0100

    week3 day2 challenge stars

---
+++ week_3/day_2/daily_challenge/daily_challenge_stars.js
@@ -0,0 +1,13 @@
+let pattern = "";
+for (let i = 0; i <= 6; i++) {
+  pattern += "* ";
+  console.log(pattern);
+}
+
+for (let i = 1; i <= 8; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += "* ";
+  }
+  console.log(row);
+}
