a73bd94f690e910401072a47b222e9b544152ef8
Aznag00 <125455539+Aznag00@users.noreply.github.com>
2025-08-28 15:21:20 +0100

    week3 day2 challenge

--- week_3/day_2/daily_challenge/challenges.js
+++ week_3/day_2/daily_challenge/challenges.js
@@ -1,5 +1,17 @@
 // ====== Daily Challenge 1
 
+const sentence = "this exercise is not bad";
+
+const wordNot = sentence.indexOf("not");
+const wordBad = sentence.indexOf("bad");
+
+if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
+  const newSentence =
+    sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+  console.log(newSentence);
+} else {
+  console.log(sentence);
+}
 // ====== Daily Challenge 2
 
 // ====== Daily Challenge 3
