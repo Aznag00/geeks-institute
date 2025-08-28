88d57ebcfd7d3c92ffa951da8d3eab27bf4a0ff1
Aznag00 <125455539+Aznag00@users.noreply.github.com>
2025-08-28 16:42:59 +0100

    week3 day3 challenges

--- week_3/day_3/daily_challenges/challenges.js
+++ week_3/day_3/daily_challenges/challenges.js
@@ -1,5 +1,32 @@
 // ====== Daily Challenge 1
+const planets = [
+  { name: "Mercury", color: "gray", moons: 0 },
+  { name: "Venus", color: "orange", moons: 0 },
+  { name: "Earth", color: "blue", moons: 1 },
+  { name: "Mars", color: "red", moons: 2 },
+  { name: "Jupiter", color: "brown", moons: 79 },
+  { name: "Saturn", color: "yellow", moons: 82 },
+  { name: "Uranus", color: "lightblue", moons: 27 },
+  { name: "Neptune", color: "darkblue", moons: 14 },
+];
 
-// ====== Daily Challenge 2
+const section = document.querySelector("section");
 
-// ====== Daily Challenge 3
+planets.forEach((planet) => {
+  const planetDiv = document.createElement("div");
+  planetDiv.classList.add("planet");
+  planetDiv.style.backgroundColor = planet.color;
+
+  const planetName = document.createElement("span");
+  planetName.textContent = planet.name;
+  planetName.classList.add("planet-name");
+  planetDiv.appendChild(planetName);
+
+  for (let i = 0; i < planet.moons; i++) {
+    const moonDiv = document.createElement("div");
+    moonDiv.classList.add("moon");
+    planetDiv.appendChild(moonDiv);
+  }
+
+  section.appendChild(planetDiv);
+});
